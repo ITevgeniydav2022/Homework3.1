f539265fee5d8e6d1d5e41b3caee05c98728a9e5
ITevgeniydav2022 <defox2022@gmail.com>
2022-11-12 14:40:45 +0300

    Сделано ДЗ Условный оператор ч.1 Задание 2

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,25 @@ public class Main {
     public static void main(String[] args) {
         // Задание 1
         System.out.println("Задание 1");
-        int clientOS = 1;
+        int clientOS = 0;
         if (clientOS <= 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        // Задание 2
+        System.out.println("Задание 2");
+        int clientDeviceYear = 2020;
+        int clientOS1 = 0;
+        if (clientOS1 <= 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS1 <= 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения, для iOS по ссылке");
+        }
+        if (clientOS1 > 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS1 > 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения, для Android по ссылке");
         }
     }
 }
